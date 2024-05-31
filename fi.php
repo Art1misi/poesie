877b8b2e518cc1a6569c3164da5b3337d2dde847
Art1misi <87003030+Art1misi@users.noreply.github.com>
2024-05-31 02:51:20 +0200

    Update fi.php

--- fi.php
+++ fi.php
@@ -10,27 +10,13 @@ background-color: lightblue;
 </head>
 <body>
 <?php 
-//$file=$_POST["name"].$_POST["email"];
 
- 
-
-/*if (file_exist("$nfile.txt"))  { 
- 
- echo "l'utente  ha gia un account registrato! se si vuole cancellare l'utente premere Reset "; 
-
-}else{*
-if($myfile = fopen("utente1.txt", "x+")){
-	echo "fatto";
-}else{
-echo "gia esistente";
-}
-fclose($myfile);*/
 $myfile=fopen("utenti.txt", "a+");
 
 $txt=$_POST["email"]."    utente     ".$_POST["name"];
 
 fwrite($myfile, $txt);
-/*echo readfile("utenti.txt");*/
+
 fclose($myfile);
 
 
